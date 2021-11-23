b6b7f90344cbce79568c2b801bd01208bc096d9a
Ricardo Roa <ricardo2roa@gmail.com>
2021-11-23 16:59:12 -0500

    Se crea componente tablero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceroComponent
+    CabeceroComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
