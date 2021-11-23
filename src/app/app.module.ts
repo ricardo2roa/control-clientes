bfbc1fbe508c5e56f41a8f437903de77e7a4c58a
Ricardo Roa <ricardo2roa@gmail.com>
2021-11-23 16:57:50 -0500

    Se crea Componente Cabecero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent
   ],
   imports: [
     BrowserModule,
