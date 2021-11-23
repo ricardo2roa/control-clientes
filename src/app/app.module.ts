7f42be6c2b1475da0bf62d3c4f7588b5155d39df
Ricardo Roa <ricardo2roa@gmail.com>
2021-11-23 17:01:55 -0500

    Se crea componentes necesarios para la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceroComponent,
-    TableroComponent
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
